019dc5e8056f9982f283ae94eb30e211cd787c64
Joco223 <jovanmihajlovic22@gmail.com>
2019-12-04 16:32:41 +0100

    Updated cfg parser and visualizer

--- source/Main.nim
+++ source/Main.nim
@@ -6,25 +6,25 @@ import Rocket/RocketsController
 
 loadMainConfig("C:/Programming/Nim/Custom-NN-Rockets/config.cfg")
 
-let width:cint           = (cint)loadWindowWidth()
-let height:cint          = (cint)loadWindowHeight()
-let rocketCount          = loadRocketCount()
-let rocketStartPosition  = vec2(loadRocketPositionX(), loadRocketPositionY())
-let rocketStartAngle     = loadRocketAngle()
-let rocketViewDistance   = loadRocketViewDistance()
-let endTick              = loadTotalTime()
-let goalPosition         = vec2(loadGoalPositionX(), loadGoalPositionY())
-let visualizerEnabled    = loadVisualizerEnabled()
-let distanceReward       = loadDistanceReward()
-let mutationEnabled      = loadMutationEnabled()
-let debugMode            = loadDebugMode()
-let interRocketCollision = loadInterRocketCollision()
-let defaultBrain         = loadDefaultBrain()
-let fpsLimit:cint        = (cint)loadFramerateLimit()
-let mutationChance       = loadMutationChance()
-let neuronCount          = loadNeuronCount() - 21 #21 here is current input+output configuration
-let maxConnectionCount   = loadMaxConnectionCount()
-let adjustementValue     = loadAdjustementValue()
+let width:cint           = (cint)loadInt("Main_Parameters", "window_width", 800)
+let height:cint          = (cint)loadInt("Main_Parameters", "window_height", 600)
+let rocketCount          = loadInt("Main_Parameters", "rocket_count", 0)
+let rocketStartPosition  = vec2(loadFloat("Main_Parameters", "rocket_position_x", 0), loadFloat("Main_Parameters", "rocket_position_y", 0))
+let rocketStartAngle     = loadFloat("Main_Parameters", "rocket_angle", 0)
+let rocketViewDistance   = loadFloat("Main_Parameters", "rocket_view_distance", 10)
+let endTick              = loadInt("Main_Parameters", "rocket_count", 10)
+let goalPosition         = vec2(loadFloat("Level_Parameters", "goal_position_x", 0), loadFloat("Level_Parameters", "goal_position_y", 0))
+let visualizerEnabled    = loadBool("Main_Parameters", "visualizer_enabled", false)
+let distanceReward       = loadFloat("Main_Parameters", "distance_reward", 1)
+let mutationEnabled      = loadBool("Main_Parameters", "mutation_enabled", false)
+let debugMode            = loadBool("Main_Parameters", "debug_mode", false)
+let interRocketCollision = loadBool("Main_Parameters", "inter_rocket_collision", false)
+let defaultBrain         = loadString("Main_Parameters", "default_brain", "")
+let fpsLimit:cint        = (cint)loadInt("Main_Parameters", "framerate_limit", 60)
+let mutationChance       = loadFloat("Main_Parameters", "mutation_chance", 0.1)
+let neuronCount          = loadInt("Main_Parameters", "neuron_count", 30) - 21 #21 here is current input+output configuration
+let maxConnectionCount   = loadInt("Main_Parameters", "max_connection_count", 1)
+let adjustementValue     = loadFloat("Main_Parameters", "adjustement_value", 1)
 
 loadWalls(loadWallConfig())
 
@@ -46,12 +46,14 @@ while window.open:
 
   while window.pollEvent event:
     if event.kind == EventType.Closed:
+      windowNN.close()
       window.close()
 
   if visualizerEnabled:
     while windowNN.pollEvent eventNN:
       if eventNN.kind == EventType.Closed:
         windowNN.close()
+        window.close()
 
   window.clear(color(50, 50, 50, 255))
 
--- source/Visualizer.nim
+++ source/Visualizer.nim
@@ -15,9 +15,8 @@ var connectionPositions = newSeq[Vector2f]()
 var connectionsInput = newSeq[seq[int]]()
 var connectionLinesBasic = newSeq[VertexArray]()
 var connectionLinesStrength = newSeq[VertexArray]()
-var neuronCircle = newCircleShape(10)
-neuronCircle.position = vec2(0, 0)
-neuronCircle.fillColor = color(200, 200, 200, 255)
+var neuronCircle: CircleShape
+var neuronSize: float
 
 proc initNNVisualizer*(x: JsonNode) =
   windowNN = newRenderWindow(videoMode(width, height), "NN Visualizer", WindowStyle.Titlebar|WindowStyle.Close, context_settings(antialiasing=8))
@@ -25,13 +24,16 @@ proc initNNVisualizer*(x: JsonNode) =
   neuronCount = nn.len()-1
   let angle = 360/neuronCount
 
+  neuronSize = (2*(width/2)*3.14) / (float)(neuronCount + 15) / 2
+  neuronCircle = newCircleShape(neuronSize)
+
   var currentAngle:float = 0
   for i in countup(0, neuronCount):
-    let posX:float = round(cos(degToRad(currentAngle)) * (height/2 - 15) - 10 + width/2)
-    let posy:float = round(sin(degToRad(currentAngle)) * (height/2 - 15) - 10 + height/2)
+    let posX:float = round(cos(degToRad(currentAngle)) * (height/2 - (neuronSize*1.5)) - neuronSize + width/2)
+    let posy:float = round(sin(degToRad(currentAngle)) * (height/2 - (neuronSize*1.5)) - neuronSize + height/2)
 
-    let posX2:float = round(cos(degToRad(currentAngle)) * (height/2 - 25) - 10 + width/2)
-    let posy2:float = round(sin(degToRad(currentAngle)) * (height/2 - 25) - 10 + height/2)
+    let posX2:float = round(cos(degToRad(currentAngle)) * (height/2 - (neuronSize*2.5)) - neuronSize + width/2)
+    let posy2:float = round(sin(degToRad(currentAngle)) * (height/2 - (neuronSize*2.5)) - neuronSize + height/2)
 
     currentAngle += angle
     positions.add(vec2(posX, posY))
@@ -53,8 +55,8 @@ proc initNNVisualizer*(x: JsonNode) =
 
   for i in countup(0, connectionPositions.len()-1):
     for connection in connectionsInput[i]:
-      var currentPosition = connectionPositions[i] + vec2(10, 10)
-      var targetPosition = connectionPositions[connection] + vec2(10, 10)
+      var currentPosition = connectionPositions[i] + vec2(neuronSize, neuronSize)
+      var targetPosition = connectionPositions[connection] + vec2(neuronSize, neuronSize)
       var vert1: Vertex
       vert1.color = color(100, 100, 255, 255)
       vert1.position = currentPosition
@@ -71,8 +73,8 @@ proc initNNVisualizer*(x: JsonNode) =
   for i in countup(0, connectionPositions.len()-1):
     if nn["neuron" & $i]["inputs"].len() != 0:
       for j in countup(0, nn["neuron" & $i]["outputIndexes"].len()-1):
-        var currentPosition = connectionPositions[i] + vec2(10, 10)
-        var targetPosition = connectionPositions[nn["neuron" & $i]["outputIndexes"][j].getInt()] + vec2(10, 10)
+        var currentPosition = connectionPositions[i] + vec2(neuronSize, neuronSize)
+        var targetPosition = connectionPositions[nn["neuron" & $i]["outputIndexes"][j].getInt()] + vec2(neuronSize, neuronSize)
 
         var averageStrength: float = nn["neuron" & $i]["inputAdjustements"][j].getFloat()
         let newAlpha:int = abs((int)(255 * averageStrength))
@@ -106,8 +108,8 @@ proc drawNN*(x: JsonNode) =
     if x["neuron" & $i]["inputs"].len() != 0:
       for j in countup(0, x["neuron" & $i]["inputs"].len()-1):
         if j < x["neuron" & $i]["inputNeurons"].len()-1:
-          var currentPosition = connectionPositions[i] + vec2(10, 10)
-          var targetPosition = connectionPositions[x["neuron" & $i]["inputNeurons"][j].getInt()] + vec2(10, 10)
+          var currentPosition = connectionPositions[i] + vec2(neuronSize, neuronSize)
+          var targetPosition = connectionPositions[x["neuron" & $i]["inputNeurons"][j].getInt()] + vec2(neuronSize, neuronSize)
 
           var currentStrength = x["neuron" & $i]["inputs"][j].getFloat()
           var newAlpha = abs((int)round(255*currentStrength))
